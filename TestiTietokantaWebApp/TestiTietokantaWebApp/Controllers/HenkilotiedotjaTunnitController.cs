08e84c0360594353fc1bbcded32ee6a93b108492
Teemu Kotajärvi <teemu.kotajarvi@stud.pointcollege.fi>
2017-11-15 10:13:26 +0200

    Entiteetti malli luotu.

---
+++ TestiTietokantaWebApp/TestiTietokantaWebApp/Controllers/HenkilotiedotjaTunnitController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestiTietokantaWebApp.Controllers
+{
+    public class HenkilotiedotjaTunnitController : Controller
+    {
+        // GET: HenkilotiedotjaTunnit
+        public ActionResult Henkilo()
+        {
+            string connStr = "Server=tcp:teemuko.database.windows.net,1433;" +
+                   "Initial Catalog=TestiTietokanta;Persist Security Info=False;User ID=Administratori;" +
+                   "Password=Cuccumia123;MultipleActiveResultSets=False;Encrypt=True;" +
+                   "TrustServerCertificate=False;Connection Timeout=30;";
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+            string sql = "SELECT * FROM henkilot WHERE Etunimi = 'Juha'";
+
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string Etunimi = reader.GetString(1);
+                string Sukunimi = reader.GetString(2);
+                
+
+                //resurssien vapautus.
+                reader.Close();
+                cmd.Dispose();
+                conn.Dispose();
+                return View(Etunimi + Sukunimi);
+            }
+        }
+    }
+}
