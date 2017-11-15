d5edadfae7ed2d278b9ed9064980553c0fa3f8a1
Teemu Kotajärvi <teemu.kotajarvi@stud.pointcollege.fi>
2017-11-15 10:39:00 +0200

    AsiakasListaus Toimii.

---
+++ Masterdetail/Masterdetail/Controllers/HenkilotController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Masterdetail.Models;
+
+namespace Masterdetail.Controllers
+{
+    public class HenkilotController : Controller
+    {
+        // GET: Henkilot
+        public ActionResult Index()
+        {
+            TestiTietokantaEntities entities = new TestiTietokantaEntities();
+
+            try
+            {
+                List<Henkilot> model = entities.Henkilot.ToList();
+                return View(model); 
+            }
+            finally
+            {
+                entities.Dispose();
+            }
+            
+        }
+    }
+}
